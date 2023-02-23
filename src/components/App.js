149dd1cf150772a9c9f2678ffaeb45e832db1a1d
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-23 23:48:30 +0530

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,83 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, { useState } from "react";
 
-const relation = (str1, str2) => {
-  let common = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (str2.indexOf(str1[i]) !== -1) {
-      common += str1[i];
-      str2 = str2.replace(str1[i], "");
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [relationship, setRelationship] = useState("");
+  
+  const handleClick = () => {
+    if (firstName === "" || secondName === "") {
+      setRelationship("Please Enter valid input");
+      return;
     }
-  }
-
-  let str1WithoutCommon = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (common.indexOf(str1[i]) === -1) {
-      str1WithoutCommon += str1[i];
+    
+    const name1 = firstName.toLowerCase();
+    const name2 = secondName.toLowerCase();
+    
+    let commonChars = "";
+    for (let i = 0; i < name1.length; i++) {
+      const char = name1.charAt(i);
+      if (name2.includes(char)) {
+        commonChars += char;
+        name2 = name2.replace(char, "");
+      }
     }
-  }
-
-  let str2WithoutCommon = "";
-  for (let i = 0; i < str2.length; i++) {
-    str2WithoutCommon += str2[i];
-  }
-
-  let totalLength = str1WithoutCommon.length + str2WithoutCommon.length;
-  switch (totalLength % 6) {
-    case 1:
-      return "Friends";
-    case 2:
-      return "Love";
-    case 3:
-      return "Affection";
-    case 4:
-      return "Marriage";
-    case 5:
-      return "Enemy";
-    case 0:
-      return "Siblings";
-    default:
-      return "";
-  }
-};
-
-const App = () => {
-  const [str1, setStr1] = useState("");
-  const [str2, setStr2] = useState("");
-  const [relationType, setRelationType] = useState("");
-
-  const handleStr1Change = (event) => {
-    setStr1(event.target.value);
+    
+    const remaining1 = name1.replace(new RegExp(`[${commonChars}]`, "g"), "");
+    const remaining2 = name2.replace(new RegExp(`[${commonChars}]`, "g"), "");
+    const sum = (remaining1.length + remaining2.length) % 6;
+    
+    switch (sum) {
+      case 0:
+        setRelationship("Siblings");
+        break;
+      case 1:
+        setRelationship("Friends");
+        break;
+      case 2:
+        setRelationship("Love");
+        break;
+      case 3:
+        setRelationship("Affection");
+        break;
+      case 4:
+        setRelationship("Marriage");
+        break;
+      case 5:
+        setRelationship("Enemy");
+        break;
+      default:
+        setRelationship("Please Enter valid input");
+    }
   };
-
-  const handleStr2Change = (event) => {
-    setStr2(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setRelationType(relation(str1, str2));
+  
+  const handleClear = () => {
+    setFirstName("");
+    setSecondName("");
+    setRelationship("");
   };
 
   return (
-    <div id="main">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            String 1:
-            <input type="text" value={str1} onChange={handleStr1Change} />
-          </label>
-        </div>
-        <div>
-          <label>
-            String 2:
-            <input type="text" value={str2} onChange={handleStr2Change} />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        {relationType === "" ? (
-          <p>Please Enter valid input</p>
-        ) : (
-          <p>Relation type: {relationType}</p>
-        )}
-      </div>
+    <div>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        data-testid="input1"
+      />
+      <input
+        type="text"
+        value={secondName}
+        onChange={(e) => setSecondName(e.target.value)}
+        data-testid="input2"
+      />
+      <button onClick={handleClick} data-testid="calculate_relationship">
+        Calculate Relationship Future
+      </button>
+      <button onClick={handleClear}>Clear</button>
+      <h3 data-testid="answer">{relationship}</h3>
     </div>
   );
-};
+}
 
 export default App;
